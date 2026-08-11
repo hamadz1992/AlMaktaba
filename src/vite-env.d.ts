aea896296d10bb102a43502b47e4de4573094017
hamadz1992 <34457332+hamadz1992@users.noreply.github.com>
2026-08-11 13:23:40 +0100

    types: add profile and window control APIs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,10 @@ declare global {
       login(username: string, password: string): Promise<any>;
       logout(): Promise<any>;
       getSession(): Promise<any>;
+      updateProfile(username: string, displayName: string): Promise<any>;
+      changeOwnPassword(oldPassword: string, newPassword: string): Promise<any>;
+      minimizeWindow(): Promise<any>;
+      closeWindow(): Promise<any>;
       listTransactions(): Promise<any[]>;
       createTransaction(input: any): Promise<any>;
       updateTransaction(id: number, input: any): Promise<any>;
@@ -13,7 +17,6 @@ declare global {
       updateProduct(id: number, input: any): Promise<any>;
       deleteProduct(id: number): Promise<any>;
       backup(): Promise<any>;
-      changeOwnPassword(oldPassword: string, newPassword: string): Promise<any>;
     };
   }
 }
